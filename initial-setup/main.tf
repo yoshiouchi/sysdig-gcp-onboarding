33ac5d666f60344bdff21b34cc779ba1ce866bdb
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2024-12-18 15:52:40 +1100

    Added initial setup

---
+++ initial-setup/main.tf
@@ -0,0 +1,33 @@
+locals {
+  project = var.GCP_PROJECT_ID
+  gcp_region = var.GCP_REGION
+  services = [
+    "sts.googleapis.com",
+    "cloudasset.googleapis.com",
+    "cloudidentity.googleapis.com",
+    "admin.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
+  ]
+}
+
+provider "google" {
+  project     = local.project
+  region      = local.gcp_region
+}
+
+resource "google_project_service" "enable_cspm_apis" {
+  project  = local.project
+
+  for_each = toset(local.services)
+  service = each.value
+  disable_on_destroy = false
+}
+
+output "enabled_projects" {
+  value = distinct([for service in local.services : google_project_service.enable_cspm_apis[service].project])
+}
+output "enabled_services" {
+  value = [for service in local.services : google_project_service.enable_cspm_apis[service].service]
+}
---
+++ initial-setup/variables.tf
@@ -0,0 +1,9 @@
+variable "GCP_PROJECT_ID" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "GCP_REGION" {
+  description = "The GCP region"
+  type        = string
+}
