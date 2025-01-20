aa94a47934ad74d47ee4a89951274c0e7abd46ed
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-01-20 11:59:27 +1100

    Added container and dns to the filter

--- deploy-terraform/log_ingestion.tf
+++ deploy-terraform/log_ingestion.tf
@@ -55,6 +55,20 @@ module "pub-sub" {
             "google.logging.v2.LoggingServiceV2.ListLogEntries"
           )
       EOT
+    },
+    {
+      name = "container.googleapis.com"
+      description = "Exclude services/methods from container.googleapis.com"
+      filter = <<EOT
+        protoPayload.serviceName = "container.googleapis.com" AND
+          protoPayload.methodName:(
+            "google.container.v1beta1.ClusterManager.ListClusters" OR
+            "google.container.v1beta1.ClusterManager.ListOperations" OR
+            "google.container.v1.ClusterManager.ListClusters" OR
+            "google.container.v1beta1.ClusterManager.GetCluster" OR
+            "google.container.v1.ClusterManager.GetCluster"
+          )
+      EOT
     }
   ]
 }
